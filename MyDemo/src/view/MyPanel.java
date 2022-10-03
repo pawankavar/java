382feae754b7dc95e37e94277b0043c6107b0dbb
pawankavar <pawankavar@gmail.com>
2022-10-03 11:32:29 +0530

    saved-03102022

--- MyDemo/src/view/MyPanel.java
+++ MyDemo/src/view/MyPanel.java
@@ -1,6 +1,10 @@
 package view;
 
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.Insets;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -12,34 +16,79 @@ public class MyPanel extends JPanel {
 	private static final long serialVersionUID = 1L;
 
 	public MyPanel() {
-		JLabel lblTitle = new JLabel("add User : ");
-		JButton btnOk = new JButton("OK");
-		JButton btnCancel = new JButton("Cancel");
-		add(lblTitle);
-		add(createFormPanel());
-		add(btnOk);
-		add(btnCancel);
+		var formLabel = new JLabel("Add User");
+		formLabel.setFont(new Font("Serif", Font.PLAIN, 30));
+		
+		setLayout(new GridBagLayout());
+		
+		var gc = new GridBagConstraints();
+		
+		gc.gridx = 0;
+		gc.gridy = 0;		
+		gc.weighty = 1; 
+		
+		add(formLabel, gc);	
+		
+		gc.weighty = 1.5;
+		gc.gridy++;
+		gc.anchor = GridBagConstraints.NORTH;
+		add(createFormPanel(), gc);
 	}
 
-	JPanel createFormPanel() {
-		JPanel p = new JPanel(new GridLayout(2, 2, 5, 5));
-
-		JLabel lbluname = new JLabel("Username : ");
-		JLabel lblPassword = new JLabel("Password : ");
-
-		JTextField txtUname = new JTextField(15);
-		JTextField txtPass = new JTextField(15);
-
-//		p.setBackground(Color.red);
-
-		p.add(lbluname);
-		p.add(txtUname);
-		p.add(lblPassword);
-		p.add(txtPass);
+	private JPanel createFormPanel() {
+		JPanel panel = new JPanel();
+		panel.setBorder(BorderFactory.createEtchedBorder());
+		var gc = new GridBagConstraints();
+		panel.setLayout(new GridBagLayout());
+	
 		
+		var nameLabel = new JLabel("Name:");
+		var passLabel = new JLabel("Password:");
 		
-		p.setBorder(BorderFactory.createEtchedBorder());
-		return p;
-
+		var nameField = new JTextField(15);
+		var passField = new JTextField(15);
+		
+		var addButton = new JButton("Save");
+		
+		var rightPad = new Insets(0, 0, 0, 10);
+		var zeroInsets = new Insets(0, 0, 0, 0);
+		
+		gc.gridy++;
+		gc.gridwidth = 1;
+		gc.weighty = 0.1;
+		
+		gc.gridx = 0;
+		gc.anchor = GridBagConstraints.LINE_END;
+		gc.insets = rightPad;
+		panel.add(nameLabel, gc);
+		
+		gc.gridx = 1;
+		gc.anchor = GridBagConstraints.LINE_START;
+		gc.insets = zeroInsets;
+		panel.add(nameField, gc);
+		
+		gc.gridy++;
+		gc.weighty = 0.1;
+		
+		gc.gridx = 0;
+		gc.anchor = GridBagConstraints.LINE_END;
+		gc.insets = rightPad;
+		panel.add(passLabel, gc);
+		
+		gc.gridx = 1;
+		gc.anchor = GridBagConstraints.LINE_START;
+		gc.insets = zeroInsets;
+		
+		panel.add(passField, gc);
+		
+		gc.gridy++;
+		gc.weighty = 30;
+		gc.gridx = 1;
+		gc.anchor = GridBagConstraints.FIRST_LINE_END;
+		
+		panel.add(addButton, gc);
+		
+		return panel;
 	}
+
 }
